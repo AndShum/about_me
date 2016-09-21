16deb0f9e262a659bd2f8dd134e876ad9392bcf3
AndShum <ashumway2@gmail.com>
2016-09-21 16:20:27 -0700

    question 7 added, working on question 6

--- app.js
+++ app.js
@@ -1,52 +1,116 @@
-"use strict";
+'use strict';
 
 
-var question1 = prompt('Was I born in Washington state?').toUpperCase();
+var answer1 = prompt('Was I born in Washington state?').toUpperCase();
+var correctAnswers = 0;
 
-if (question1 === 'YES' || question1 === 'Y'){
-  alert("Correct!");
-  console.log('user answered question 1 correctly');
-} else {
-  alert("Incorrect");
-  console.log('user answered quesion 1 incorrectly');
+// if (answer1 === 'YES' || answer1 === 'Y'){
+//   alert('Correct!');
+//   correctAnswers += 1;
+//   console.log('user answered question 1 correctly.  Currently ' + correctAnswers + ' of 7 answers correct.');
+// } else {
+//   alert('Incorrect');
+//   console.log('user answered quesion 1 incorrectly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// }
+//
+// var answer2 = prompt('Have I ever been to Europe?').toUpperCase();
+//
+// if (answer2 === 'NO' || answer2 === 'N'){
+//   alert('Correct!');
+//   correctAnswers += 1;
+//   console.log('user answered question 2 correctly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// } else {
+//   alert('Incorrect');
+//   console.log('user answered question 2 incorrectly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// }
+//
+// var answer3 = prompt('Am I under 25 years old?').toUpperCase();
+//
+// if (answer3 === 'NO' || answer3 === 'N'){
+//   alert('Correct!');
+//   correctAnswers += 1;
+//   console.log('user answered question 3 correctly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// } else {
+//   alert('Incorrect');
+//   console.log('ser answered question 3 incorrectly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// }
+//
+// var answer4 = prompt('Did I have a mixed breed dog when I was growing up?').toUpperCase();
+//
+// if (answer4 === 'YES' || answer4 === 'Y'){
+//   alert('Correct!');
+//   correctAnswers += 1;
+//   console.log('user answered question 4 correctly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// } else {
+//   alert('Incorrect');
+//   console.log('user answered question 4 incorrectly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// }
+//
+// var answer5 = prompt('Did I attend a public elementary school?').toUpperCase();
+//
+// if (answer5 === 'NO' || answer5 === 'N'){
+//   alert('Correct!');
+//   correctAnswers += 1;
+//   console.log('user answered question 5 correctly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// } else {
+//   alert('Incorrect');
+//   console.log('user answered question 5 incorrectly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// }
+
+var numChoice = 0;
+var answer = false;
+var counter = 0;
+
+while (answer === false && counter < 4){
+  var answer6 = parseInt(prompt('How many cars have I crashed?'));
+  for( var i = 0; i < 4; i ++ ){
+    if (answer6 === numChoice){
+      alert('Correct!');
+      correctAnswers += 1;
+      console.log('user answered question 5 correctly. Currently ' + correctAnswers + ' of 7 answers correct.');
+      console.log('user answer to question 6 was ' + numChoice);
+      answer = true;
+    }
+  } if (answer === false){
+    alert('Incorrect.');
+    counter ++
+  }
 }
 
-var question2 = prompt('Have I ever been to Europe?').toUpperCase();
+// if (answer6 === 0){
+//   alert('Correct!');
+//   correctAnswers += 1;
+//   console.log('user answered question 6 correctly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// } else {
+//   alert('Incorrect');
+//   console.log('user answered question 6 incorrectly. Currently ' + correctAnswers + ' of 7 answers correct.');
+// }
 
-if (question2 === 'NO' || question2 === 'N'){
-  alert("Correct!");
-  console.log('user answered question 2 correctly');
-} else {
-  alert("Incorrect");
-  console.log('user answered question 2 incorrectly');
+var multiChoice = ['MEXICO', 'CANADA', 'THAILAND', 'JAPAN'];
+
+
+while (answer === false && counter < 6) {
+  var answer7 = prompt('What countries have I visited outside of the U.S.?').toUpperCase();
+  for ( i = 0; i < multiChoice.length; i ++) {
+    if( answer7 === multiChoice[i]){
+      alert('Correct!');
+      correctAnswers += 1;
+      console.log('user answered question 7 correctly. Currently ' + correctAnswers + ' of 7 answers correct.');
+      console.log('user answer for question7 was ' + answer7);
+      answer = true;
+    }
+  }
+  if (answer === false) {
+    alert('Incorrect');
+    counter ++;
+  }
 }
 
-var question3 = prompt('Am I under 25 years old?').toUpperCase();
 
-if (question3 === 'NO' || question3 === 'N'){
-  alert("Correct!");
-  console.log('user answered question 3 correctly');
+if (correctAnswers <= 3){
+  alert(correctAnswers + ' out 7 correct? You don\'t know me at all do you?');
+} else if (correctAnswers <= 5){
+  alert(correctAnswers + ' out 7 correct. Not too bad, well done.');
 } else {
-  alert("Incorrect");
-  console.log('ser answered question 3 incorrectly');
-}
-
-var question4 = prompt('Did I have a mixed breed dog when I was growing up?').toUpperCase();
-
-if (question4 === 'YES' || question4 ==='Y'){
-  alert("Correct!");
-  console.log('user answered question 4 correctly');
-} else {
-  alert("Incorrect");
-  console.log('user answered question 4 incorrectly');
-}
-
-var question5 = prompt('Did I attend a public elementary school?').toUpperCase();
-
-if (question5 === 'NO' || question5 === 'N'){
-  alert("Correct!")
-  console.log('user answered question 5 correctly');
-} else {
-  alert("Incorrect");
-  console.log('user answered question 5 incorrectly');
+  alert(correctAnswers + ' out of 7 correct...why do you know so much about me?');
 }
--- welcome.js
+++ welcome.js
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 
 var name = prompt('Hello, what is your name?');
 alert("Hello " + name + ", welcome to my website!");
